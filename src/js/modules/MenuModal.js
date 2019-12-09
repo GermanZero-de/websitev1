3f795e463f97e84560036885d4317f989f5ae04a
alexs-p-ysbm <alexs.p.ysbm@gmail.com>
2019-12-09 18:21:12 +0200

    header yellow menu 2.0

--- src/js/modules/MenuModal.js
+++ src/js/modules/MenuModal.js
@@ -1,2 +1,49 @@
 export default class MenuModal {
+  constructor() {
+    document.getElementById('menu-btn').addEventListener('click', () => {
+      this.toggleMenuModal(false);
+    }, false);
+
+    document.getElementById('menu-btn-close').addEventListener('click', () => {
+      this.toggleMenuModal(true);
+    }, false);
+    const menuList = document.getElementById("menu-screen-list");
+    const menuItems = document.querySelectorAll("#menu-screen-list>.sublist");
+    for (const item of menuItems ) {
+      item.addEventListener('click', (e) => {
+        this.toggleMenuModalList(e, menuList, menuItems);
+      }, false);
+    }
+  }
+
+  toggleMenuModalList(e, menuList, menuItems) {
+    if (e.target.children[0]) {
+      if (e.target.children[0].classList.contains('active')) {
+        e.target.classList.remove('active')
+        e.target.children[0].classList.remove('active')
+        menuList.classList.remove('active')
+      } else {
+        for (const item of menuItems ) {
+          item.classList.remove('active')
+          item.children[0].classList.remove('active')
+        }
+        e.target.classList.add('active');
+        e.target.children[0].classList.add('active');
+        menuList.classList.add('active')
+      }
+    }
+  }
+
+  toggleMenuModal(isMenuOpened) {
+    let menuScreen = document.getElementById('menu-screen');
+    if (isMenuOpened) {
+      document.body.style.overflow = 'auto';
+      menuScreen.classList.remove('active');
+    } else {
+      document.body.style.overflow = 'hidden';
+      menuScreen.classList.add('active');
+    }
+  }
+
+
 }
