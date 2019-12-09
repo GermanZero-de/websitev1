b663272ad5301cc5249a2a5e114ec4cb9e41ba88
alexey m <alexey.m.ysbm@gmail.com>
2019-12-10 01:07:29 +0200

    ueber-uns markup, mach-mit progress

--- webpack/webpack.dev.conf.js
+++ webpack/webpack.dev.conf.js
@@ -9,7 +9,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   devServer: {
     contentBase: baseWebpackConfig.externals.paths.dist,
     port: 3000,
-    hot: true,
+    // hot: true,
     overlay: {
       warnings: true,
       errors: true,
