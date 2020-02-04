c1403c8f820d0302319c2d219f8038701039f1bd
Fabian <freelance@fabianfrank.it>
2020-02-04 22:04:09 +0100

    Validate image

--- src/js/modules/ProfileGenerator.js
+++ src/js/modules/ProfileGenerator.js
@@ -1,3 +1,31 @@
+const errorTypes = {
+  TYPE_ERROR: 'TYPE_ERROR',
+  SRC_ERROR: 'SRC_ERROR',
+  SIZE_ERROR: 'SIZE_ERROR',
+  LOAD_ERROR: 'LOAD_ERROR',
+};
+
+const createError = (message, name) => {
+  const e = new Error(message);
+  e.name = name;
+  return e;
+};
+
+const validateImage = (src, size = 4 * 1024 * 1024) => {
+  if (!src) {
+    throw createError('no src for image', errorTypes.SRC_ERROR);
+  }
+  if (!src.type.match(/image.*/)) {
+    throw createError('File is not an image.', errorTypes.TYPE_ERROR);
+  }
+  if (src.size > size) {
+    throw createError(
+      `The image is too large. It should be smaller than ${size / 1024 / 1024} MB`,
+      errorTypes.SIZE_ERROR,
+    );
+  }
+};
+
 export default class ProfileGenerator {
   constructor() {
     this.fileUploadInput = document.querySelector('.js-profile-upload-file-dialog');
@@ -12,6 +40,7 @@ export default class ProfileGenerator {
   uploadImage(event) {
     const src = event.target.files[0];
     try {
+      validateImage(src, 6 * 1024 * 1024);
       this.loadImage(src);
     } catch (e) {
       alert(e.message);
